103d2239a550db28e220bfb10a9661582970ded4
Jake <66682228+spacebuilder49@users.noreply.github.com>
2023-05-18 17:55:47 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "spoderman":return img`
+........ffffffff..........
+.......ff2222222ff........
+......ff2222222222fff.....
+......f222f2222222222ff...
+.....ff2222222222f2222f...
+.....f2222222222222222ff..
+.....f2222222222222222f...
+.....f2222222222222222f...
+.....f2222222222222222f...
+.....f222222fff2222222f...
+.....ff2222222222222ff....
+.......ffff2222222ff8f....
+.....ff888fffffffff888f...
+...fff8888f2222222f8888f..
+..ff88888ff2222222ff888f..
+.f888888ff222f22f22f888f..
+.f8888fff22222ff222f8888f.
+f88888f2222222ff222f8888f.
+f8888f222222ffffff2f8888f.
+f8888f22222222ff222f8888f.
+f8888f2222222ffff22f8888f.
+f8888f22222222ff2222f888f.
+f8888f2222222fffff222f88f.
+f8888f2222222f222f222f88f.
+f8888f222222222222222f88f.
+f8888f222222222222222f88f.
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
